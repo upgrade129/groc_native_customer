4b79f3fba2c10eec3d9ffdd59d20ec6f9a60ac3a
rvigneshw <vigneshw1811@gmail.com>
2020-08-23 21:36:57 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text,
+  Item,
+  Input,
+  Segment,
+  Card,
+  CardItem,
+  Thumbnail,
+} from "native-base";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize empty state here
+    this.state = {
+      products: [],
+    };
+  }
+  componentWillMount() {
+    // It's best to use your api call on componentWillMount
+    this.getProducts();
+  }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  getProducts() {
+    fetch("https://groc-api.herokuapp.com/products")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          products: responseJson,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left />
+          <Body>
+            <Title>Groc</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Search products" />
+          </Item>
+        </Header>
+        <Content>
+          {this.state.products.map((product, i) => {
+            return (
+              <Card>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={{ uri: product.image }} />
+                    <Body>
+                      <Text>{product.name}</Text>
+                      <Text note>
+                        Price:{product.price}/{product.quantity}
+                        {product.unit}
+                      </Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem>
+                  <Left>
+                    <Button transparent>
+                      <Text>Add To Cart</Text>
+                    </Button>
+                  </Left>
+                  <Body>
+                    <Button transparent>
+                      <Icon active name="chatbubbles" />
+                      <Text>4 Comments</Text>
+                    </Button>
+                  </Body>
+                  <Right>
+                  <Button transparent>
+                      <Text>Add To Cart</Text>
+                    </Button>
+                  </Right>
+                </CardItem>
+              </Card>
+            );
+          })}
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button vertical>
+              <Icon name="apps" />
+              <Text>Home</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="camera" />
+              <Text>Orders</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="navigate" />
+              <Text>Profile</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
