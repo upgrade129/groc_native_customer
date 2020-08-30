99c14645291bec8f7be7de79939581f04c23e869
rvigneshw <vigneshw1811@gmail.com>
2020-08-30 12:54:07 +0530

    Added screen to list shops

---
+++ Components/ShopCard.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text,
+  Item,
+  Input,
+  Segment,
+  Card,
+  CardItem,
+  Thumbnail,
+} from "native-base";
+
+export default function ShopCart(props) {
+  return (
+    <Card>
+      <CardItem>
+        <Left>
+          {/* <Thumbnail source={{ uri: props.image }} /> */}
+          <Body>
+            <Text>{props.shop_id}</Text>
+            <Text note>Shop Score:{props.shop_score}</Text>
+          </Body>
+        </Left>
+      </CardItem>
+      <CardItem>
+        <Button full success>
+          <Text>Buy From this Shop</Text>
+        </Button>
+      </CardItem>
+    </Card>
+  );
+}
--- Pages/Cart.js
+++ Pages/Cart.js
@@ -25,8 +25,65 @@ import { createStackNavigator } from "@react-navigation/stack";
 import AsyncStorage from "@react-native-community/async-storage";
 
 import CartCard from "../Components/CartCard";
+import ShopCard from "../Components/ShopCard";
 import BottomTab from "../Components/BottomTab";
 import AppBar from "../Components/AppBar";
+
+const data = {
+  shop_product_id_array: [
+    {
+      shop_id: "5f3630a5fee41a005ff2d047",
+      available_products: [
+        {
+          id: "5f317f6227b9d004ac3313b2",
+          price: 23,
+        },
+        {
+          id: "5f317fc627b9d004ac3313b3",
+          price: 60,
+        },
+        {
+          id: "5f31801227b9d004ac3313b4",
+          price: 50,
+        },
+      ],
+      shop_score: 3,
+    },
+    {
+      shop_id: "5f392e2a7b25630017007fec",
+      available_products: [
+        {
+          id: "5f317f6227b9d004ac3313b2",
+          price: 23,
+        },
+        {
+          id: "5f31801227b9d004ac3313b4",
+          price: 50,
+        },
+      ],
+      shop_score: 2,
+    },
+    {
+      shop_id: "5f393a820afaa700f8545c98",
+      available_products: [
+        {
+          id: "5f317f6227b9d004ac3313b2",
+          price: 23,
+        },
+        {
+          id: "5f317fc627b9d004ac3313b3",
+          price: 60,
+        },
+      ],
+      shop_score: 2,
+    },
+  ],
+  product_id_array: [
+    "5f317f6227b9d004ac3313b2",
+    "5f317fc627b9d004ac3313b3",
+    "5f31801227b9d004ac3313b4",
+  ],
+};
 
 class Cart extends Component {
   constructor(props) {
@@ -79,33 +136,35 @@ class Cart extends Component {
           full
           success
           onPress={() => {
-            var data={
-              ordered_items:this.state.cart_items
-            }
+            var data = {
+              ordered_items: this.state.cart_items,
+            };
             fetch("http://groc-api.herokuapp.com/find-best-shop", {
               method: "post",
-              mode: 'no-cors', // no-cors, *cors, same-origin
+              mode: "no-cors", // no-cors, *cors, same-origin
               headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
+                Accept: "application/json",
+                "Content-Type": "application/json",
               },
-              body:JSON.stringify({
-                ordered_items:this.state.cart_items
+              body: JSON.stringify({
+                ordered_items: this.state.cart_items,
               }),
             })
               .then(function (response) {
                 return response.json();
               })
               .then(function (responseData) {
-                console.log(responseData)
+                console.log(responseData);
               })
               .catch((error) => {
                 console.error(error);
               });
-              console.log(JSON.stringify({
-                ordered_items:this.state.cart_items
-              }))
-            // this.props.navigation.navigate("Details");
+            console.log(
+              JSON.stringify({
+                ordered_items: this.state.cart_items,
+              })
+            );
+            this.props.navigation.navigate("Details");
           }}
           title="Open Modal"
         >
@@ -122,7 +181,17 @@ function ModalScreen({ navigation }) {
   //TODO list shops in this modal
   return (
     <Container>
-      <Text>{this.state.cart_items}</Text>
+      {/* <Text>{this.state.cart_items}</Text> */}
+      <Content>
+      {data.shop_product_id_array.map((shop, i) => {
+        return (
+          <ShopCard
+          shop_id={shop.shop_id}
+          shop_score={shop.shop_score}
+          />
+        );
+      })}
+      </Content>
       <Button full dark onPress={() => navigation.goBack()}>
         <Text>Go back</Text>
       </Button>
